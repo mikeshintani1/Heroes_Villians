90609206251b1b5f32a1fe0fba2116aae35c0b04
michael shintani <mikeshintani@yahoo.com>
2022-04-04 09:29:02 -0700

    recreated project, improper syntax used earlier to create unusable files project_title . missing

--- heroes_villians_project/urls.py
+++ heroes_villians_project/urls.py
@@ -1,4 +1,4 @@
-"""heroes_villians URL Configuration
+"""heroes_villians_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
