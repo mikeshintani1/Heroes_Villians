64e9682fb0b246063d7c3d5bc704482421ce0cdc
michael shintani <mikeshintani@yahoo.com>
2022-04-04 11:21:09 -0700

    models, serializers super_types_id corrected on django and postman

--- supers/serializers.py
+++ supers/serializers.py
@@ -4,7 +4,7 @@ from .models import Supers
 class SupersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Supers
-        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
+        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_types_id']
         depth = 1
     
     super_types_id = serializers.IntegerField(write_only=True)
